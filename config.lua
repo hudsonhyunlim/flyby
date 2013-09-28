6f2d4daa32fc9164607b0acd8876717e6583c64d
darkfoxtokoyami <darkfoxfurre@gmail.com>
2013-09-28 12:35:45 -0700

    Changed config.lua - Added per-device resolution

--- config.lua
+++ config.lua
@@ -1,12 +1,27 @@
-application =
-{
-    content =
-    {
-        width = 640,
-        height = 960,
-        scale = "letterbox",
-        xAlign = "left",
-        yAlign = "top",
+if ( display.pixelHeight / display.pixelWidth > 1.72 ) then
+	application =
+	{
+		content =
+		{
+			width = 540,
+			height = 960,
+			scale = "letterbox",
+			xAlign = "left",
+			yAlign = "top",
 
-    }
-}
+		}
+	}
+else
+	application =
+	{
+		content =
+		{
+			width = 640,
+			height = 960,
+			scale = "letterbox",
+			xAlign = "left",
+			yAlign = "top",
+
+		}
+	}
+end
