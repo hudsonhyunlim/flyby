eefeba2c6c7a57b403100ac5f283dd82424ef892
darkfoxtokoyami <darkfoxfurre@gmail.com>
2013-09-28 15:10:18 -0700

    Added counter for monsters and monster types on screen

--- monster.lua
+++ monster.lua
@@ -11,13 +11,19 @@ local monster = {}
 
 -- | VARIABLE DECLARATIONS | --
 local _Physics = require "gamephysics"				-- This is the object that handles the world's physics
-local _MonsterCount = 0								-- Count of monsters on screen
+local _MonsterCount =	{							-- Count of monsters on screen
+							["zepp"] = 0,
+							["total"] = 0
+						}
 local _Monsters = {}
 
 function monster.init()
-	local t_monster = display.newImage("ika.png")
+	local t_monster = display.newImage("images/zepp.png")
 	t_monster.x = display.contentWidth 
 	t_monster.y = 50
+	t_monster.id = "zepp"
+	_MonsterCount[t_monster.id] = _MonsterCount[t_monster.id] + 1
+	_MonsterCount["total"] = _MonsterCount["total"]  + 1
 	table.insert(_Monsters, t_monster)
 end
 
@@ -25,11 +31,15 @@ function monster.scroll()
 	for i=1, #_Monsters do
 		_Monsters[i]:translate(-_Physics.sceneSpeed, 0)
 		if _Monsters[i].x < 0 then
+			print(_MonsterCount[_Monsters[i].id])
+			_MonsterCount[_Monsters[i].id] = _MonsterCount[_Monsters[i].id] - 1
+			print(_MonsterCount[_Monsters[i].id])
+			_MonsterCount["totals"] = _MonsterCount["total"] - 1
 			_Monsters[i]:removeSelf()
 			_Monsters[i] = nil
 		end
 	end
-	print(#_Physics)
+	
 	
 end
 
