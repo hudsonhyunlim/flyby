baf44549e117199d487acbab09c7c91adc1a613d
darkfoxtokoyami <darkfoxfurre@gmail.com>
2013-09-28 12:18:25 -0700

    Changed plane image

--- plane.lua
+++ plane.lua
@@ -10,7 +10,7 @@
 local plane = {}
 -- | VARIABLE DECLARATIONS | --
 local _Physics = require "gamephysics"				-- This is the object that handles the world's physics
-local _Plane = display.newImage( "ika.png" )	-- The player character object
+local _Plane = display.newImage( "images/PlayerPlane.png" )	-- The player character object
 local _Ascend = false							-- Is the user currently pressing to fly?
 local _Ceiling = display.newRect(0, -1, display.contentWidth, 1)	-- Prevents plane from returning to its people
 
@@ -31,7 +31,7 @@ function _Plane:timer(event)
 	local vx, vy = _Plane:getLinearVelocity()
 	if _Ascend == true then
 		if vy > -500 then
-			_Plane:applyForce(0, -100, 0, 0)
+			_Plane:applyForce(0, -1000, 0, 0)
 		end
 	end
 	timer.performWithDelay(5, _Plane)
