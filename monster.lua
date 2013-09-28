2d8408d09cebcb1628db2adb752e36a94d54904e
darkfoxtokoyami <darkfoxfurre@gmail.com>
2013-09-28 16:28:32 -0700

    Added code for determining how to spawn monsters

--- monster.lua
+++ monster.lua
@@ -17,6 +17,24 @@ local _MonsterCount =	{							-- Count of monsters on screen
 						}
 local _Monsters = {}
 
+local function addMonster()
+	local t_monster = display.newImage("images/zepp.png")
+	local physicsData = (require "physicseditor.zepp").physicsData(1.0)
+	_Physics.addBody( t_monster,"static", physicsData:get("zepp") )
+	t_monster.x = (display.contentWidth + (t_monster.width / 2))
+	t_monster.y = 50
+	t_monster.id = "zepp"
+	_MonsterCount[t_monster.id] = _MonsterCount[t_monster.id] + 1
+	_MonsterCount["total"] = _MonsterCount["total"]  + 1
+	table.insert(_Monsters, t_monster)
+end
+
+local function spawnMonster()
+	if _MonsterCount["zepp"] < 2 then
+	end
+end
+
+
 function monster.init()
 	local t_monster = display.newImage("images/zepp.png")
 	local physicsData = (require "physicseditor.zepp").physicsData(1.0)
@@ -29,6 +47,7 @@ function monster.init()
 	table.insert(_Monsters, t_monster)
 end
 
+
 function monster.scroll()
 	for i=1, #_Monsters do
 		_Monsters[i]:translate(-_Physics.sceneSpeed, 0)
@@ -36,14 +55,17 @@ function monster.scroll()
 			print(_MonsterCount[_Monsters[i].id])
 			_MonsterCount[_Monsters[i].id] = _MonsterCount[_Monsters[i].id] - 1
 			print(_MonsterCount[_Monsters[i].id])
-			_MonsterCount["totals"] = _MonsterCount["total"] - 1
+			_MonsterCount["total"] = _MonsterCount["total"] - 1
 			_Monsters[i]:removeSelf()
 			_Monsters[i] = nil
 		end
-	end
-	
-	
+		if _MonsterCount["total"] < 2 then
+			spawnMonster()
+		end
+	end	
 end
+
+
 
 
 return monster
